4b961160094428dcc1a3978e006018a4b4a72d42
Виталий Травин <vtravin@nologostudio.ru>
2023-07-18 17:22:22 +0600

    +2 tasks

--- index.js
+++ index.js
@@ -287,10 +287,33 @@ const jadenCasedStrings = function (str) {
 
     return res;
 };
-console.log(jadenCasedStrings("How can mirrors be real if our eyes aren't real"));
+// console.log(jadenCasedStrings("How can mirrors be real if our eyes aren't real"));
 // 2 вариант
 String.prototype.toJadenCase = function () {
     return this.split(' ')
         .map((item) => item[0].toUpperCase() + item.slice(1))
         .join(' ');
 };
+
+/*
+   12. Digital Root
+
+  Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+    Examples
+        16  -->  1 + 6 = 7
+        942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+        132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+        493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+function digitalRoot(n) {
+    let nArr = n.toString().split('');
+    if (nArr.length <= 1) {
+        return n;
+    } else {
+        let arrNum = nArr.map(Number);
+        return digitalRoot(arrNum.reduce((total, amount) => total + amount, 0));
+    }
+}
+
+// console.log(digitalRoot(942));
